a85e4f14be10e9738a037284d082f3c4f8574fbc
psfonseka <psfonseka@gmail.com>
2019-08-02 17:23:29 -0400

    Passed all tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,8 +11,8 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
-
-var requestHandler = function(request, response) {
+const messagesArr = [];
+const requestHandler = function(request, response) {
   // Request and Response come from node's http module.
   //
   // They include information about both the incoming request, such as
@@ -28,22 +28,47 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
-
   // See the note below about CORS headers.
-  var headers = defaultCorsHeaders;
+  const headers = defaultCorsHeaders;
+  if(request.method === 'GET') {
+    if (request.url !== "/classes/messages") {
+      const statusCode = 404;
+      response.writeHead(statusCode, headers);
+      response.end("Error: Not Found!");
+    } else {
+      const statusCode = 200;
+      const msgs = JSON.stringify({'results': messagesArr});
+      response.writeHead(statusCode, headers); 
+      response.end(msgs);
+    }
+  } else if(request.method === 'POST') {
+    const statusCode = 201;
+    let newmsg = '';
+    request.on('data', (cb) => {
+      newmsg += cb;
+      messagesArr.push(JSON.parse(newmsg));
+      const msgs = JSON.stringify({'results': messagesArr});
+      response.writeHead(statusCode, headers); 
+      response.end(msgs);
+    });
+  } else {
+    const statusCode = 200;
+    const msgs = JSON.stringify({'results': messagesArr});
+    response.writeHead(statusCode, headers); 
+    response.end(msgs);
+  }
+
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'application/json';
+  //headers['Content-Type'] = 'text/plain';
+  //var msgs = JSON.stringify(messagesArr); 
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  //response.writeHead(statusCode, headers);
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +77,7 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+  //response.end(msgs);
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -68,7 +93,8 @@ var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'text/plain'
 };
 
 module.exports.requestHandler = requestHandler;
