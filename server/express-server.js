20c188df904510af395fd38f91def7ec38ce37dc
psfonseka <psfonseka@gmail.com>
2019-08-03 09:58:57 -0400

    Added working urls with both nothing and classes/messages

--- server/express-server.js
+++ server/express-server.js
@@ -6,20 +6,21 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('./client'))
+
 
 const messages = [];
-app.get('/classes/messages', (req, res) => {
+app.get('/+(classes/messages)?', (req, res) => {
     res.type('json');
     res.send(JSON.stringify({'results': messages}));
     console.log('get request');
 });
 
-app.post('/classes/messages', (req, res) => {
+app.post('/+(classes/messages)?', (req, res) => {
     messages.unshift(req.body);
     res.type('json');
     res.send(JSON.stringify({'results': messages}));
     console.log('post request');
-    console.log(messages);
 });
 
 app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
