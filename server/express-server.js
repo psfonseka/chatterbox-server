d1f70c65a3c691ad51b1b38dba5dc959d0a52956
psfonseka <psfonseka@gmail.com>
2019-08-03 09:34:18 -0400

    Implemented working server using express

--- server/express-server.js
+++ server/express-server.js
@@ -9,14 +9,17 @@ app.use(cors());
 
 const messages = [];
 app.get('/classes/messages', (req, res) => {
-    res.send(messages)
+    res.type('json');
+    res.send(JSON.stringify({'results': messages}));
     console.log('get request');
 });
 
 app.post('/classes/messages', (req, res) => {
-    messages.push(req.body);
-    res.send(messages);
+    messages.unshift(req.body);
+    res.type('json');
+    res.send(JSON.stringify({'results': messages}));
     console.log('post request');
+    console.log(messages);
 });
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'));
+app.listen(port, () => console.log('Example app listening on port ' + port + '!'));
